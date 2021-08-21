ef022246f6df0cf518c9cd53e49347928d661d47
moMagdy4 <mohamed.magdy.migo11@gmail.com>
2021-08-21 03:18:13 +0200

    Added Animation

--- app/src/main/java/com/example/todo/Register.java
+++ app/src/main/java/com/example/todo/Register.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,6 +18,7 @@ public class Register extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.register_layout);
 
 

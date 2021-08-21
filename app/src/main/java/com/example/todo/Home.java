ca6f49db11b55dd4b9c0a7422a0607690c8f72d1
rengo540 <ar6029676@gmail.com>
2021-08-21 02:31:05 +0200

    added register and connectioon firebase

---
+++ app/src/main/java/com/example/todo/Home.java
@@ -0,0 +1,14 @@
+package com.example.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_layout);
+    }
+}
---
+++ app/src/main/java/com/example/todo/Register.java
@@ -0,0 +1,41 @@
+package com.example.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Register extends AppCompatActivity {
+    private EditText regEmail , regPassword ;
+    private Button regBtn ;
+    private TextView regTologin;
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_layout);
+
+
+        toolbar=findViewById(R.id.registerToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Register");
+
+
+        regEmail=findViewById(R.id.registerEmail);
+        regPassword=findViewById(R.id.registerPassword);
+        regBtn=findViewById(R.id.registerButton);
+        regTologin=findViewById(R.id.registerPageQuestion);
+
+
+
+
+
+
+
+    }
+
+}
